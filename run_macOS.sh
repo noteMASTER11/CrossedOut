723b0c08d7c6e5854c063d1d6c586121ff1fd222
Daniil Derbenyov <smailik.ne@yandex.ru>
2024-08-08 02:27:05 +0000

    modified:   run_macOS.sh

--- run_macOS.sh
+++ run_macOS.sh
@@ -3,6 +3,9 @@
 # Установка кодировки UTF-8 (по умолчанию в macOS)
 export LANG=en_US.UTF-8
 
+# Определение директории, в которой находится скрипт
+SCRIPT_DIR=$(dirname "$0")
+
 # Проверка наличия Python по указанному пути
 PYTHON_PATH="/usr/local/bin/python3"
 if [ ! -f "$PYTHON_PATH" ]; then
@@ -27,23 +30,23 @@ fi
 
 # Создание виртуального окружения
 echo "Создание виртуального окружения..."
-$PYTHON_PATH -m venv venv
+$PYTHON_PATH -m venv "$SCRIPT_DIR/venv"
 
 # Проверка, что виртуальное окружение создано
-if [ ! -f "venv/bin/activate" ]; then
+if [ ! -f "$SCRIPT_DIR/venv/bin/activate" ]; then
     echo "Ошибка создания виртуального окружения."
     exit 1
 fi
 
 # Активация виртуального окружения
 echo "Активация виртуального окружения..."
-source venv/bin/activate
+source "$SCRIPT_DIR/venv/bin/activate"
 
 # Установка зависимостей
 echo "Установка зависимостей..."
 pip install --upgrade pip
-pip install -r requirements.txt
+pip install -r "$SCRIPT_DIR/requirements.txt"
 
 # Запуск вашего приложения
 echo "Запуск приложения..."
-python linkedindumper.py
+python "$SCRIPT_DIR/linkedindumper.py"
